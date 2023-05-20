028a6a30a0719be3f09ce8fd9728624d8886f12a
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-05-20 21:17:57 +0200

    Refactor

--- shell/src/components/MicroFrontends.tsx
+++ shell/src/components/MicroFrontends.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const ReactComponent = ({ manifest }: any) => {
+const MicroFrontends = ({ manifest }: any) => {
   const entrypoint = manifest["src/MicroFrontend.jsx"]["file"];
-  const MicroFrontend = React.lazy(
+  const MicroFrontendA = React.lazy(
     () => import(`http://localhost:7100/${entrypoint}`)
   );
 
   return (
     <>
-      <MicroFrontend />
+      <MicroFrontendA />
     </>
   );
 };
 
-export default ReactComponent;
+export default MicroFrontends;
